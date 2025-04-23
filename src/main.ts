a5e940f3128d3eb527fecece22a8d4c3b1eb354d
mohamed mrdoctor <mhamedfekry0@gmail.com>
2025-04-23 11:45:19 +0200

    Remove ConsoleLogs ..

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,5 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`Server is running on http://localhost:${port}`);
-
-  
 }
 bootstrap();
